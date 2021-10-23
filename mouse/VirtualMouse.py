6e780da333c0f93be675fa106cb94e35123594f7
Szymon <szymonmarciniak22@gmail.com>
2021-10-23 19:25:30 +0200

    Adding virtual mouse documentation

--- mouse/VirtualMouse.py
+++ mouse/VirtualMouse.py
@@ -6,11 +6,13 @@ import pyautogui
 
 
 def virtual_mouse():
-    ##########################
+    """
+    This function is responsible for control mouse (moveing and clicking). You control mouse with your hand.
+    """
+    
     wCam, hCam = 640, 480
     frameR = 100 # Frame Reduction
     smoothening = 7
-    #########################
 
     pTime = 0
     plocX, plocY = 0, 0
@@ -19,71 +21,71 @@ def virtual_mouse():
     cap = cv2.VideoCapture(0)
     cap.set(3, wCam)
     cap.set(4, hCam)
-    detector = htm.handDetector(maxHands=1)
+    detector = htm.HandDetector(maxHands=1)
     wScr, hScr = 1920, 1080
-    # print(wScr, hScr)
     while True:
 
-        def click(x,y):
+        def click(x: int,y: int) -> None:
             pyautogui.moveTo(x, y)
             pyautogui.click(x=x, y=y)
 
-        # 1. Find hand Landmarks
+        # Find hand Landmarks
         success, img = cap.read()
-        img = detector.findHands(img)
-        lmList, bbox = detector.findPosition(img)
-        # 2. Get the tip of the index and middle fingers
+        img = detector.find_hands(img)
+        lmList, bbox = detector.find_position(img)
+
+        # Get the tip of the index and middle fingers
         if len(lmList) != 0:
             x1, y1 = lmList[8][1:]
-            # print(x1, y1, x2, y2)
-        
-        # 3. Check which fingers are up
-            fingers = detector.fingersUp()
-        # print(fingers)
+            
+            
+        # Check which fingers are up
+            fingers = detector.fingers_up()
             cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
             (255, 0, 255), 2)
-            # 4. Only Index Finger : Moving Mode
+
+            # Only Index Finger : Moving Mode
             if fingers[1] == 1 and fingers[2] == 0:
-                # 5. Convert Coordinates
+
+                # Convert Coordinates
                 x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
                 y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
-                # 6. Smoothen Values
+
+                # Smoothen Values
                 clocX = plocX + (x3 - plocX) / smoothening
                 clocY = plocY + (y3 - plocY) / smoothening
             
-                # 7. Move Mouse
+                # Move Mouse
                 x = wScr - clocX
                 x = int(x)
                 y = int(clocY)
-
                 pyautogui.moveTo(x, y)
-                #autopy.mouse.move(wScr - clocX, clocY)
                 cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
                 plocX, plocY = clocX, clocY
                 
-            # 8. Both Index and middle fingers are up : Clicking Mode
+            # Both Index and middle fingers are up : Clicking Mode
             if fingers[1] == 1 and fingers[2] == 1:
-                # 9. Find distance between fingers
-                length, img, lineInfo = detector.findDistance(8, 12, img)
-                print(length)
-                # 10. Click mouse if distance short
+
+                # Find distance between fingers
+                length, img, lineInfo = detector.find_distance(8, 12, img)
+               
+                # Click mouse if distance short
                 if length < 40:
                     cv2.circle(img, (lineInfo[4], lineInfo[5]),
                     15, (0, 255, 0), cv2.FILLED)
-                    #autopy.mouse.click()
                     x = wScr - clocX
                     x = int(x)
                     y = int(clocY)
                     click(x, y)
 
-            
-        # 11. Frame Rate
+        # Frame Rate
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
         cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,
         (255, 0, 0), 3)
-        # 12. Display
+
+        # Display
         #cv2.imshow("Image", img)
         cv2.waitKey(1)
 
--- mouse/modules/HandTrackingModule.py
+++ mouse/modules/HandTrackingModule.py
@@ -1,12 +1,19 @@
 
+from tkinter import Image
+from typing import List, Tuple
 import cv2
 import mediapipe as mp
 import time
 import math
 
 
-class handDetector():
+class HandDetector():
+    """
+    This class includes modules responsible for: finding hands and their positions, counting fingers up and the distance between them.
+    """
+
     def __init__(self, mode=False, maxHands=2,model_complexity=1, detectionCon=0.6, trackCon=0.5):
+
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
@@ -18,10 +25,25 @@ class handDetector():
         self.mpDraw = mp.solutions.drawing_utils
         self.tipIds = [4, 8, 12, 16, 20]
 
-    def findHands(self, img, draw=True):
+    def find_hands(self, img: Image, draw=True) -> Image:
+        """
+        This function finding hands in given image
+
+        Parametrs
+        ---------
+        img: Image
+
+        draw: Bool
+            If you want to draw landmarks, set this parametr to True
+
+        Return
+        ------
+        img: Image
+            Image with marked hands
+        """
+
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-        # print(results.multi_hand_landmarks)
 
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
@@ -31,13 +53,31 @@ class handDetector():
 
         return img
 
-    def findPosition(self, img, handNo=0, draw=True):
+    def find_position(self, img: Image,draw=True) -> List:
+        """
+        This function is responsible for finding hand position in given image
+
+        Parametrs
+        ---------
+        img: Image
+
+        draw: Bool
+            If you want to draw circle and rectangle, set this parametr to True
+        
+        Return
+        ------
+        lmlist: List
+            List of ID and position each finger
+        bbox: Tuple
+            Tuple of minimal nad maximal x and y position of all fingers
+        """
+
         xList = []
         yList = []
         bbox = []
         self.lmList = []
         if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
+            myHand = self.results.multi_hand_landmarks[0]
             for id, lm in enumerate(myHand.landmark):
                 # print(id, lm)
                 h, w, c = img.shape
@@ -59,10 +99,19 @@ class handDetector():
 
         return self.lmList, bbox
 
-    def fingersUp(self):
-        fingers = []
-        # Thumb
+    def fingers_up(self) -> List:
+        """
+        This function counts fingers up
 
+        Return
+        ------
+        fingers: List
+            The list contains a boolean value for each finger. 0 if the finger is down, 1 if the finger is up 
+        """
+
+        fingers = []
+
+        # Thumb
         if len(self.lmList) != 0:
             if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
                 fingers.append(1)
@@ -81,7 +130,36 @@ class handDetector():
 
         return fingers
 
-    def findDistance(self, p1, p2, img, draw=True,r=15, t=3):
+    def find_distance(self, p1: int, p2: int, img: Image, draw=True,r=15, t=3) -> float:
+        """
+        This function is responsible for counting the distance between two fingers
+
+        Parametrs
+        ---------
+        p1: Int
+            Id of finger number 1 
+        p2: Int
+            Id of finger number 2
+        img: Image
+            Image
+        draw: Bool
+            Set to True if you want to draw circles and a line between these two fingers 
+        r: Int
+            Radius of drawing circle
+        t: Int
+            Thickness of drawing line
+        
+        Return
+        ------
+        length: Float
+            Length between two fingers
+        img: Image
+            Image
+        list: List
+            List of first and second finger positions and the position of the middle between them
+
+
+        """
         x1, y1 = self.lmList[p1][1:]
         x2, y2 = self.lmList[p2][1:]
         cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
@@ -97,14 +175,17 @@ class handDetector():
 
 
 def main():
+    """
+    Turn on entire HandDetector class and display camera preview 
+    """
     pTime = 0
     cTime = 0
     cap = cv2.VideoCapture(0)
-    detector = handDetector()
+    detector = HandDetector()
     while True:
         success, img = cap.read()
-        img = detector.findHands(img)
-        lmList, bbox = detector.findPosition(img)
+        img = detector.find_hands(img)
+        lmList, bbox = detector.find_position(img)
         if len(lmList) != 0:
             print(lmList[4])
 
